2279a3b5f2afb06ce5709ca1bfc56b914b9b1b56
Caitlin Brooks <64147934+caitlinbrooks03@users.noreply.github.com>
2021-12-03 13:31:00 -0600

    Assignment Roles & Stylesheet

---
+++ assignment.php
@@ -0,0 +1,45 @@
+<html>
+    <header>
+            <title>Group Roles</title>
+            <link rel="stylesheet" href="farkle.css">
+        </header>
+    <body>
+        <div class = "navbar">
+            <ul>
+                <li><a href="../aboutMeAngularJS.html">About Me</a></li>
+                <li><a href="../index.html">Home</a></li>
+            </ul>
+        </div>
+        <div class = "intro">
+        </div>
+        <div class = "content">
+            
+            <h2>Group Roles</h2>
+            <table class = "center">
+                <tr>
+                    <th>Member</th>
+                    <th>Role</th>
+                </tr>
+                <tr>
+                    <td>Caitlin Brooks</td>
+                    <td>              </td>
+                </tr>
+                <tr>
+                    <td>Aries Qualey</td>
+                    <td>              </td>
+                </tr>
+            </table>
+            
+            <br>
+            
+            <form method = "post" action = "farkle.php">
+                <input type = "submit" value = "Let's Play!">
+            </form>
+            <form method = "post" action = "index.php">
+                <input type = "submit" value = "Farkle Home">
+            </form>
+        
+        </div>
+    </body>
+
+<html>
